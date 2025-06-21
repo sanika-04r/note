5db4b89b2b869b384456b09fdec7e94617843917
sanika ransing <sanikaransing04@gmail.com>
2025-06-21 19:37:15 +0530

    intial commit

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ require("dotenv").config()
 const cors = require("cors")
 const app = express()
 app.use(express.json()) // req.body madhe data takto hi linr
-app.use(cors({ origin: "http://localhosta:5173", credentials: true })) // cookie satthi true yena janya sathi
+app.use(cors({ origin: "http://localhost:5173", credentials: true })) // cookie satthi true yena janya sathi
 app.use("/api/notes", require("./routes/note.routes"))
 
 mongoose.connect(process.env.MONGO_URL)
